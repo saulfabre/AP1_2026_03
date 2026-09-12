5efd5411b21d46b05b198e7dede655ab47c2976c
saulfabre <saulfabre2@gmail.com>
2026-09-12 15:24:08 -0400

    Crear servicio de Libros

---
+++ Services/LibrosService.cs
@@ -0,0 +1,68 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using RegistroLibrosBlazor.Models;
+
+namespace RegistroLibrosBlazor.Services;
+
+public class LibrosService(
+    IDbContextFactory<Contexto> contextFactory
+) : Aplicada1.Core.IService<Libros, int>
+{
+    public async Task<Libros?> Buscar(int libroId)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .FirstOrDefaultAsync(l => l.LibroId == libroId);
+    }
+
+    public async Task<bool> Insertar(Libros libro)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        contexto.Libros.Add(libro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int libroId)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .Where(l => l.LibroId == libroId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(Libros libro)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        contexto.Update(libro);
+        return await contexto
+            .SaveChangesAsync() > 0;       
+    }
+
+    public async Task<List<Libros>> GetList(Expression<Func<Libros, bool>> criterio)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<bool> Existe(int libroId)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        return await contexto.Libros
+            .AnyAsync(l => l.LibroId == libroId);
+    }
+
+    public async Task<bool> Guardar(Libros libro)
+    {
+        if (!await Existe(libro.LibroId))
+        {
+            return await Insertar(libro);
+        }
+        else
+        {
+            return await Modificar(libro);
+        }
+    }
+}
