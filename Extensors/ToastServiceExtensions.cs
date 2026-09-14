200ed1a302e05f20696b6faa7fdabe510ba72d49
saulfabre <saulfabre2@gmail.com>
2026-09-14 06:12:15 -0400

    Crear extensión para ToastService

---
+++ Extensors/ToastServiceExtensions.cs
@@ -0,0 +1,31 @@
+using BlazorBootstrap;
+
+namespace RegistroLibrosBlazor.Extensors;
+
+public static class ToastServiceExtensions
+{
+    public static ToastMessage ShowToast(this ToastService toastService, ToastType toastType, string title, string? customMessage = null) {
+        var message = new ToastMessage() {
+            Type = toastType,
+            Title = title,
+            Message = customMessage ?? $"A las {DateTime.Now.ToString("hh:mm tt")}"
+        };
+        toastService.Notify(message);
+        return message;
+    }
+
+    public static ToastMessage ShowSuccess(this ToastService toastService, string? customMessage = null,
+        string title = "Success") {
+        return toastService.ShowToast(ToastType.Success, title, customMessage);
+    }
+
+    public static ToastMessage ShowWarning(this ToastService toastService, string? customMessage = null,
+        string title = "Warning") {
+        return toastService.ShowToast(ToastType.Warning, title, customMessage);
+    }
+
+    public static ToastMessage ShowError(this ToastService toastService, string? customMessage = null,
+        string title = "Error") {
+        return toastService.ShowToast(ToastType.Danger, title, customMessage);
+    }
+}
